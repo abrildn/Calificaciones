b5162d0a38786be4cbdd1b1c320f7f6d41b3dc1f
Abril <abrudomingueznoya@gmail.com>
2025-09-09 13:43:35 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Calificaciones
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese la calificación para aprobar el curso:");
+            string linea = Console.ReadLine();
+            int aprob = int.Parse(linea);
+
+            Console.WriteLine("Ingrese la nota calificación para ir a recuperatorio:");
+            linea = Console.ReadLine();
+            int recup = int.Parse(linea);
+
+            Console.WriteLine("Ingrese su nota");
+            linea = Console.ReadLine();
+            int nota = int.Parse(linea);
+
+            if (nota < recup)
+            {
+                Console.WriteLine("Usted desaprobó la materia");
+            }
+            else
+            {
+                if (nota >= aprob)
+                {
+                    Console.WriteLine("Usted aprobó la materia");
+                }
+                else
+                {
+                    Console.WriteLine("Debe ir a recuperatorio");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
